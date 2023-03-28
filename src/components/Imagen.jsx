c64921c92af78a82e866b070ca04d174af275a68
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-28 15:44:15 -0500

    puse boton de redireccion a i agen de alta calidad

--- src/components/Imagen.jsx
+++ src/components/Imagen.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 
 const Imagen = ({imagen}) => {
 
-    const {largeImageURL, likes, previewURL, tags, view} = imagen
+    const {largeImageURL, likes, previewURL, tags, views} = imagen
     return ( 
 
         <div className='col-12 col-sm-6 col-md-4 col-lg-3 mb-5'>
             <div className='card'>
                 <img src={previewURL} alt={tags} />
+
+                <div className='card-body'>
+                    <p className='card-text'>{likes} Me gusta</p>
+                    <p className='card-text'>{views} Vistas </p>
+                </div>
+
+                <div className='card-footer'>
+                    <a
+                        href={largeImageURL}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='btn btn-primary btn-block'
+                    >Ver Imagen</a>
+                </div>
             </div>
         </div>
         );
